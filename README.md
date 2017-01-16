ed785399ce4a7cb6d3021c37cbcdec3cae9f6ab3
kepocnhh <kepocnhh@gmail.com>
2017-01-17 00:39:04 +0300

    create project

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Cudgel
 Cudgel project on JavaFX
+
+# Config
+	applicationId "stan.cudgel"
+	versionCode 1701170028
+	versionName "0.001"
+
+# Build information
+## build
+```
+javac -sourcepath ./src/main/java -d bin -classpath lib/* ./src/main/java/stan/cudgel/App.java
+```
+
+## build css
+```
+javafxpackager -createbss -srcdir ./src/main/css -outdir bin/css -srcfiles cudgel.css -v
+```
+
+## copy res
+### Windows
+```
+xcopy .\src\main\res .\bin\res /E
+```
+
+## run
+```
+java -classpath lib/*;bin stan.cudgel.App
+```
---
+++ src/main/java/stan/cudgel/App.java
@@ -0,0 +1,28 @@
+package stan.cudgel;
+
+import javafx.application.Application;
+
+import javafx.geometry.Rectangle2D;
+
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App
+    extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setAlwaysOnTop(true);
+        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+        //primaryStage.setScene(new MainScene(screen.getWidth(), screen.getHeight()));
+        primaryStage.show();
+    }
+}
